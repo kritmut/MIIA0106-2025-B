6fd7851d3a9cfaaceae31b245af3f625441a3f7e
kritmut <6811860009@mut.ac.th>
2025-11-23 17:17:08 +0700

    การทดลองที่2

---
+++ lab2/TEST01/TEST01.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+	std::cout << "Hello LAB2\n";
+    std::cout << "Name Krittapad Sankhan\n";
+    std::cout << "Course: MIIA0106\n";
+    std::cout << "Email:6811860009@mut.ac.th\n";
+    return 0;
+}
+
