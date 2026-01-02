014cd1491b593be467966ac89d52f39c819abb43
kritmut <6811860009@mut.ac.th>
2026-01-02 17:06:36 +0700

    Lab7 ข้อ 1-6

---
+++ lab7/Lab701/Lab701.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int x = 10;
+    int* p = &x;
+
+    // TODO
+    cout << " Value of x  =\t" << x << endl;
+    cout << " Value of *P =\t" << *p << endl;
+
+    return 0;
+}
